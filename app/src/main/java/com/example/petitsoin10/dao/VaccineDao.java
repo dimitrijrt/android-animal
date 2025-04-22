a9223129fc0003790af7c9564ada1c481055603d
dimitrijrt <dimitrijacquart@gmail.com>
2025-04-22 18:59:23 +0200

    config bdd

--- app/src/main/java/com/example/petitsoin10/dao/VaccineDao.java
+++ app/src/main/java/com/example/petitsoin10/dao/VaccineDao.java
@@ -3,16 +3,23 @@ package dao;
 import java.util.List;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Upsert;
+import androidx.room.Query;
 
 import com.example.petitsoin10.data.entity.Vaccine;
 
 public interface VaccineDao
 {
 
+
+    @Query("SELECT * FROM vaccine WHERE animalId = :animalId")
     LiveData<List<Vaccine>> getAllVaccinesByAnimalId(long animalId);
 
+    @Upsert
     void addVaccine(Vaccine vaccine);
 
+    @Delete
     void deleteVaccine(Vaccine vaccine);
 
 
